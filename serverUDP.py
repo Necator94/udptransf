f9365daa722c9044e61aa1aef9d89315cbd7cadc
Andrei <blblb@gms.com>
2015-12-04 21:29:00 +0100

    file transfered without distortions

--- serverUDP.py
+++ serverUDP.py
@@ -7,7 +7,7 @@ import os
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-host = "localhost";
+host = "10.42.0.15";
 port = 5000;
  
 s.bind((host, port))
@@ -16,15 +16,18 @@ s.bind((host, port))
 
 #wr = os.fdopen(sys.stdout.fileno(), 'wb' )
 
-
+reply = 'foobar'
 
 while(1) :
         d = s.recvfrom(500)
         reply = d[0]
         addr = d[1]     
+        if (reply  == "CLOSE"):
+                break
+
       	sys.stdout.write(reply)
 	 # s.sendto(reply, addr )         
        # print reply
           
-   
+s.close()
     
