f1bd49033536df4193434384a93bd7755d2489d5
Ivan Matveev <necator94@gmail.com>
2015-12-04 13:47:47 +0100

    file descriptor created

--- test.py
+++ test.py
@@ -3,26 +3,9 @@ import io
 import struct
 import os
 
-#sys.stdin = open(sys.stdin.fileno(), 'r', <new settings>)
-#data = open(sys.stdin.read())
-#data = sys.stdin.read(16)
-#f = sys.stdin.read()
-#keke = f.read
 
-#reader = io.open(sys.stdin.fileno())
-#reader = io.open(sys.stdin.read())
+newin = os.fdopen(sys.stdin.fileno(), 'r' )
 
-#file = open(sys.stdin.read, 'r')
-#bytes  = file.read( )
+kek = newin.read(6)
+print kek
 
-#buffer=[5]
-#line = sys.stdin.read()
-#buffer.append(line)
-
-
-newin = os.fdopen(sys.stdin.fileno(), 'r', 5)
-i = 0
-while i<10:
-	kek = newin.read(5)
-	print kek
-	i = i + 1
