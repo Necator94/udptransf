25f34c5bf492926ac38ad694d973b4e46a07a7b6
Ivan Matveev <necator94@gmail.com>
2015-12-06 10:02:31 +0100

    original without crc

--- clientUDP.py
+++ clientUDP.py
@@ -7,7 +7,6 @@ import struct
 import os
 import crcmod
 
-fun = crcmod.mkCrcFun('4812730177')
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 fd = os.fdopen(sys.stdin.fileno(), 'rb' )
 
@@ -19,8 +18,6 @@ data = "foo bar"
 while (data):
 
         data = fd.read(1600)
-	msg = fun(data)
-	print bin(msg)
 	s.sendto(data, (host, port))
        # d = s.recvfrom(1024)
        # reply = d[0]
