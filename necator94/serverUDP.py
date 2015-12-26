82113800409159c3aab3db153c38700bd643ab17
Ivan <necator94@gmail.com>
2015-12-26 12:05:34 +0100

    huinya

--- necator94/serverUDP.py
+++ necator94/serverUDP.py
@@ -21,17 +21,14 @@ def defTripTime(addr, t) :
 	try :
 		indata, addr = s.recvfrom(13)
         	if (indata == 'triptimedelay') :
-            		t = (time.time() - t0) * 10
-			
-			
+            		t = (time.time() - t0) * 2 
 			return t
 	except socket.timeout:
          	s.sendto('triptimedelay', addr)
 		try :
 			indata, addr = s.recvfrom(13)
                		if (indata == 'triptimedelay') :
-	                	t = (time.time() - t0) * 10
-				
+	                	t = (time.time() - t0) * 2
 				return t
 		except socket.timeout:
 			s.sendto('triptimedelay', addr)
@@ -49,58 +46,85 @@ host = '0.0.0.0'
 port = 5000;
 indata = 'foo bar'
 lostPackets = 'ACK'
-t = 3
+t = 0.02 
 sizeOfBlock = 100
 
 s.bind((host, port))
+
 while(indata) :
-	Ncycles = 1
+	Ncycles = 0
 	i = 0
 	dataArray = [None] * sizeOfBlock
 	numbersArray = []
         while (i < sizeOfBlock) :
-		indata, addr = s.recvfrom(1632)
-
-		if (indata == "CLOSE"):
-			exit(0)
-
-		packetIndex, indata = splitting(indata)
-		dataArray[packetIndex] = indata
-		if (packetIndex != i) :
-			Ncycles = 0
+		try :
+			indata, addr = s.recvfrom(1632)
+			packetIndex, indata = splitting(indata)
+			dataArray[packetIndex] = indata
+			if (indata == "CLOSE"):
+				exit(0)
+		
+		except socket.timeout :
 			lostPackets = lostPackets + str(i) + ' '
 			Ncycles = Ncycles + 1
 		i = i + 1
-	
-	t = defTripTime(addr, t)
+#	t = defTripTime(addr, t)
 
 	s.sendto(lostPackets, addr)
+	print lostPackets, '   list of lost'
 	s.settimeout(t)
 	l = 0
-	while (l < Ncycles) :
-		
+	print Ncycles, 'Ncycles'
+	if (Ncycles == 0):
 		try :
 			indata, addr = s.recvfrom(1632)
-			if (indata != 'ACK RECIEVED') :
-				packetIndex, indata = splitting(indata)
-				dataArray[packetIndex] = indata
-		except socket.timeout:
-			s.sendto(lostPackets, addr)
-			try : 
+                        if (indata != 'ACK RECIEVED') :
+ 	                	packetIndex, indata = splitting(indata)
+                        	print packetIndex
+                               	dataArray[packetIndex] = indata
+               	except socket.timeout:
+                        s.sendto(lostPackets, addr)
+                       	try :
+                        	indata, addr = s.recvfrom(1632)
+                        	if (indata != 'ACK RECIEVED') :
+               		         	packetIndex, indata = splitting(indata)
+                                	print packetIndex
+                                	dataArray[packetIndex] = indata
+ 
+                    	except socket.timeout:
+                       		s.sendto(lostPackets, addr)
+ 		
+	else:
+		while (l < Ncycles) :
+		
+			try :
 				indata, addr = s.recvfrom(1632)
-               			if (indata != 'ACK RECIEVED') :
-               				packetIndex, indata = splitting(indata)
-                     			dataArray[packetIndex] = indata
+				if (indata != 'ACK RECIEVED') :
+			
+					packetIndex, indata = splitting(indata)
+					print packetIndex, ' - try 1'
+					dataArray[packetIndex] = indata
 			except socket.timeout:
-		        	s.sendto(lostPackets, addr)
+				s.sendto(lostPackets, addr)
+				try : 
+					indata, addr = s.recvfrom(1632)
+               				if (indata != 'ACK RECIEVED') :
+						packetIndex, indata = splitting(indata)
+                     				print packetIndex, ' - try 2'
+						dataArray[packetIndex] = indata
+					
+				except socket.timeout:
+			        	s.sendto(lostPackets, addr)
 		l = l + 1
+#	print l, ' - l'
+
 	k = 0
-	while (k < sizeOfBlock) :
+#	while (k < sizeOfBlock) :
 #		if (dataArray[k] == None) :
 #			exit(0)
 		
-		sys.stdout.write(dataArray[k])
-		k = k + 1
+#		sys.stdout.write(str(dataArray[k]))
+#		k = k + 1
           
 s.close()
   
