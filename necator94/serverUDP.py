755ae657b4bb4d6a0b32b404d751f2bc5f421106
Ivan <necator94@gmail.com>
2015-12-29 16:19:01 +0100

    hz

--- necator94/serverUDP.py
+++ necator94/serverUDP.py
@@ -5,14 +5,14 @@ import io
 import struct
 import os
 import time
-
+#------------------------------------------------------------------------
 def splitting(indata):
 	innumber = indata [ : 32] 
 	indata = indata [32 : ]       
 	binNumber = int(innumber, 2)
 	packetIndex = int(binNumber) 
 	return packetIndex,indata
-
+#------------------------------------------------------------------------
 def defTripTime(addr, t) :
 	s.settimeout(t)
 	t0 = time.time()
@@ -29,6 +29,7 @@ def defTripTime(addr, t) :
 		except socket.timeout:
 			t0 = time.time()
 	         	s.sendto('triptimedelay', addr)
+#------------------------------------------------------------------------
 def lookForNone(dataArray) :
 	lostPackets = 'ACK'
 	n_cycles = 0	
@@ -37,9 +38,11 @@ def lookForNone(dataArray) :
 			lostPackets = lostPackets + str(index) + ' '
 			n_cycles = n_cycles + 1
 	return lostPackets, n_cycles
+#------------------------------------------------------------------------
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 logs = open('logs.txt', 'w')
+
 host = '0.0.0.0'
 port = 5000;
 indata = 'foo bar'
@@ -61,7 +64,6 @@ while(indata) :
 				logs.write('CLOSE recieved'+'\n'+'exit(0)'+'\n')
 				exit(0)
 		
-			s.settimeout(t)
 			packetIndex, indata = splitting(indata)
 			dataArray[packetIndex] = indata
 			logs.write(str(i)+'   - number of iteration|    '+ str(packetIndex)+'   - index of recieved packet'+'\n')
@@ -71,12 +73,13 @@ while(indata) :
 			break		
 	logs.write('END of main while'+'\n')
 	t = defTripTime(addr, t)
+	s.settimeout(t)
 	logs.write(str(t)+'....triptime'+ '\n')
 	lostPackets, n_cycles = lookForNone(dataArray)
 	logs.write('lookForNone|   '+str(lostPackets)+'  - lostPackets|    '+ str(n_cycles)+'   - n_cycles'+'\n')
 	s.sendto(lostPackets, addr)
 	logs.write(str(lostPackets)+'....lost packets sent to client after cycle 1'+'\n')
-	s.settimeout(t)
+	
 	if (n_cycles == 0):
  		logs.write('if '+str(n_cycles)+' == 0'+ '\n') 
 		while (True):
@@ -89,23 +92,10 @@ while(indata) :
 				s.sendto(lostPackets, addr)
 				logs.write(str(lostPackets)+'.....timeout in hueta, packet list sent again'+ '\n')
 	
-#				try:
-#					indata, addr = s.recvfrom(1432)
-#					logs.write(str(indata)+'.....positive ACK from client2 '+ '\n')
-#				except socket.timeout:
-#					logs.write(str(lostPackets)+'.....2 timeout in hueta, packet list sent again 2'+ '\n')
-#					try:
- #                               		indata, addr = s.recvfrom(1432)
-  #                              		logs.write(str(indata)+'.....positive ACK from client3 '+ '\n')
-#					except socket.timeout:
-#						logs.write('ERROR positive ACK was not recieved '+ '\n')
-#						exit(0)
 	else: 		
-		logs.write('else '+str(n_cycles)+' > 0'+ '\n')	
+		logs.write('else ' + str(n_cycles) + ' > 0' + '\n')	
 		while (n_cycles > 0) :
 			logs.write('begin of ebota'+ '\n')
-			lostPackets, n_cycles = lookForNone(dataArray)
-			logs.write('lookForNone|   '+str(lostPackets)+'  - lostPackets|    '+ str(n_cycles)+'   - n_cycles'+'\n')
 			try :
 				indata, addr = s.recvfrom(1432)
 				if (indata != 'ACK RECIEVED') :
@@ -118,18 +108,9 @@ while(indata) :
 			except socket.timeout:
 				s.sendto(lostPackets, addr)
 				logs.write(str(lostPackets)+'.....first timeout in ebota, packet list sent again 2'+ '\n')
-				try : 
-					indata, addr = s.recvfrom(1432)
-               				if (indata != 'ACK RECIEVED') :
-						packetIndex, indata = splitting(indata)
-						logs.write(str(packetIndex)+'.....index of recieved lost packet 2 attempt'+ '\n')
-						dataArray[packetIndex] = indata
-					if (indata == 'ACK RECIEVED') :
-						logs.write(str(indata)+'.....positive ACK from client2 '+ '\n'+'break'+'\n')
-						break
-				except socket.timeout:
-					s.sendto(lostPackets, addr)
-					logs.write(str(lostPackets)+'.....2 timeout in ebota, packet list sent again 3'+ '\n')
+	
+		lostPackets, n_cycles = lookForNone(dataArray)
+		logs.write('lookForNone|   '+str(lostPackets)+'  - lostPackets|    '+ str(n_cycles)+'   - n_cycles'+'\n')
 	logs.write('WRITING to file'+ '\n')
 	k = 0
 	while (k < sizeOfBlock) :
