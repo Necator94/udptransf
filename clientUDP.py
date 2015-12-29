50b202ade3d5172b7c47d339fe49c75d4d0470ec
Ivan <necator94@gmail.com>
2015-12-29 20:28:35 +0100

    rep is norm

---
+++ clientUDP.py
@@ -0,0 +1,72 @@
+#clientUDP
+
+import socket  
+import sys
+import io
+import struct
+import os
+#import crcmod
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+fd = os.fdopen(sys.stdin.fileno(), 'rb' )
+
+#host = "10.42.0.15";
+host = 'localhost'
+port = 5000;
+indata = "foo bar"
+
+while (indata) :
+	
+	i = 0 
+	a = []					# Buffer creation	
+	while (i < 100) :
+	
+		indata = fd.read(1600)
+
+		#Packets numeration
+		bin_i = bin(i)
+		cleanNumber = bin_i [2 : ]
+		outString = cleanNumber + indata
+		lengthOfNumber = len(cleanNumber)
+
+		#Filling free space in the beginning of packet by zeros	
+		while lengthOfNumber < 32 :
+			outString = '0' + outString
+			lengthOfNumber = lengthOfNumber + 1
+	
+		a.append(outString)			# Adding of new element
+		
+		s.sendto(outString, (host, port))	# Sendign of outString (packet)
+		
+		i = i + 1
+	
+		#********************Recieving******************************	
+	recvListOfAcks, addr = s.recvfrom(1632) # Define nessecary size of buffer!!!!!!!!!!!!
+		
+	ackIdentificator = recvListOfAcks [ : 3] 
+	recvListOfAcks = recvListOfAcks [3 : ]
+	recvListOfAcks = recvListOfAcks.split()
+	if (ackIdentificator == 'ACK') :
+		print recvListOfAcks
+				
+
+
+			
+#			recvN = int(reply)
+		#	s.sendto(a[recvN], (host, port))
+	#		d = s.recvfrom(100)
+#			reply = d[0]
+			
+#			if reply != 'ACK2':
+#				s.sendto(a[recvN], (host, port))
+#			else :
+#				print 'ACK2'	
+				
+#		else :
+#			print 'ACK'
+
+
+s.sendto("CLOSE", (host, port))
+
+fd.close()
+s.close()
--- test3.py
+++
@@ -1,4 +0,0 @@
-valueTo1={"a","b","c"}
-
-if valueTo1.has_key("a"):
-        print "Found key in dictionary"
