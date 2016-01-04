7430a0bcdd5e46ef1ba37e2a056a757fb16565a3
Ivan <necator94@gmail.com>
2016-01-04 16:40:14 +0100

    server.py modified before testing in lab

--- serverUDP.py
+++ serverUDP.py
@@ -15,8 +15,8 @@ def defTripTime(addr, t):
 			indata, addr = s.recvfrom(13)
        			if (indata == 'triptimedelay') :
             			t = (time.time() - t0)
-				if t < 0.02:
-					t *= 100 
+				if t < 0.2:
+					t *= 2 
 				return t
 				break
 		except socket.timeout:
@@ -51,7 +51,7 @@ logs = open('logs.txt', 'w')
 host = '0.0.0.0'
 port = 5000;
 indata = 'foo bar'
-t = 2.2 
+t = 1.5 
 sizeOfBlock = 400
 
 s.bind((host, port))
@@ -64,13 +64,15 @@ while indata:
 	logs.write('BEGIN of main while'+'\n')
         while i < sizeOfBlock:
 		try :
+		#	s.settimeout(2 * t)
 			indata, addr = s.recvfrom(1432)
+			s.settimeout(t)
 			if indata == "CLOSE":
 				logs.write('CLOSE recieved'+'\n'+'exit(0)'+'\n')
 				exit(0)
 		
-			dataArray[int(int(indata[:32], 2))] = indata[32:]
-			logs.write(str(i)+'   - number of iteration|    '+ (str(int(int(indata[:32], 2))))+'   - index of recieved packet'+'\n')
+			dataArray[int(int(indata[:10], 2))] = indata[10:]
+			logs.write(str(i)+'   - number of iteration|    '+ (str(int(int(indata[:10], 2))))+'   - index of recieved packet'+'\n')
 			i += 1	
 		except socket.timeout :
 			logs.write(str(i)+'....amount of recieved packets, timeout reached')
@@ -91,8 +93,8 @@ while indata:
 			try :
 				indata, addr = s.recvfrom(1432)
 				if indata != 'ACK RECIEVED':
-					logs.write(str(int(int(indata[:32], 2))) + '.....index of recieved lost packet 1 attempt'+ '\n')
-					dataArray[int(int(indata[:32], 2))] = indata[32:]
+					logs.write(str(int(int(indata[:10], 2))) + '.....index of recieved lost packet 1 attempt'+ '\n')
+					dataArray[int(int(indata[:10], 2))] = indata[10:]
 			except socket.timeout:
 				s.sendto(lostPackets, addr)
 				logs.write(str(lostPackets)+'.....first timeout in ebota, packet list sent again 2'+ '\n')
@@ -100,18 +102,19 @@ while indata:
 			lostPackets, n_cycles = lookForNone(dataArray)
 			logs.write('lookForNone|   '+str(lostPackets)+'  - lostPackets|    '+ str(n_cycles)+'   - n_cycles'+'\n')
 	if n_cycles == 0:
-		s.sendto(ackToClient(t), addr)
-		logs.write(str(ackToClient(t))+'....ack + timeout'+'\n')
  		logs.write('if '+str(n_cycles)+' == 0'+ '\n') 
+		s.sendto('SACK', addr)
+		logs.write('SACK'+'....ack to client'+'\n')
 		while True:
 			try:
 				indata, addr = s.recvfrom(1432)
 				logs.write(str(indata)+'.....positive ACK from client1 '+ '\n')
-				if indata == 'ACK RECIEVED' or len(indata[32:]) != 0:
+				if indata == 'ACK RECIEVED' or len(indata[10:]) !=0 :
 					break	
 			except socket.timeout:
-				s.sendto(ackToClient(t), addr)
-				logs.write(str(lostPackets)+'.....timeout in hueta, packet list sent again'+ '\n')
+				s.sendto('SACK', addr)
+				logs.write('SACK'+'.....timeout in hueta, packet list sent again'+ '\n')
+	
 	logs.write('WRITING to file'+ '\n')
 	k = 0
 	while k < sizeOfBlock:
